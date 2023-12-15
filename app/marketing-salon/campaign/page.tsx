2736afebda05ad4485d94db151c0b3ef1fa4aa5e
¨LuisEUM¨ <luiseum95@gmail.com>
2023-12-15 14:21:37 +0100

    completing json and adding more tabs

--- app/marketing-salon/campaign/page.tsx
+++ app/marketing-salon/campaign/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import TailwindGrid from "@/components/grid/TailwindGrid";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardsPressable from "@/components/cards/Cards";
-import { postersCardsES, postersCardsCA } from '@/lib/helpers/postersCards';
+import { postersCardsES, postersCardsCA } from "@/lib/helpers/postersCards";
+import { Tabs, Tab } from "@nextui-org/tabs";
+import { Card, CardBody } from "@nextui-org/react";
 
 const sideMenu = {
   name: "Plan de Enero",
@@ -16,44 +18,125 @@ const sideMenu = {
   ],
 };
 
-
+let tabs = [
+  {
+    id: "es",
+    label: "Español",
+    content: postersCardsES,
+  },
+  {
+    id: "ca",
+    label: "Catalán",
+    content: postersCardsCA,
+  },
+];
 
 export default function MarketingSalon() {
   const searchParams = useSearchParams();
   const [tab, setTab] = useState(searchParams.get("tab"));
   const [year, setYear] = useState(searchParams.get("year"));
   const [month, setMonth] = useState(searchParams.get("month"));
+  const [category, setCategory] = useState(searchParams.get("category"));
+  const [categoryType, setCategoryType] = useState(searchParams.get("type"));
+  // const [categoryTypeLanguage, setCategoryTypeLanguage] = useState(
+  //   searchParams.get("language")
+  // );
+  const [list, setList] = useState<any[]>([]);
 
-  console.log(postersCardsES)
+  useEffect(() => {
+    if (category === "physicalContent") {
+      if (categoryType === "posters") {
+        // if (categoryTypeLanguage === "es") {
+        //   setList(postersCardsES);
+        // } else if (categoryTypeLanguage === "ca") {
+        //   setList(postersCardsCA);
+        // }
+      }
+    }
+  }, [category, categoryType]);
+
   return (
     <>
       <TailwindGrid fullSize>
         <div className="col-span-1 col-start-1 col-end-2 h-screen fixed w-2/12  top-0 z-30 border-r box-border border-zinc-500 bg-white-950/40 backdrop-blur-lg bg-clip-padding backdrop-filter opacity-75 hidden lg:block">
-          <ul  aria-label={`Plan de Marketing ${month && month?.charAt(0).toUpperCase() + month?.slice(1)} ${year ?? ''}`} className="w-full h-full z-30 flex items-center   justify-center flex-col gap-y-10">
+          <ul
+            aria-label={`Plan de Marketing ${
+              month && month?.charAt(0).toUpperCase() + month?.slice(1)
+            } ${year ?? ""}`}
+            className="w-full h-full z-30 flex items-center   justify-center flex-col gap-y-10"
+          >
             <strong className="font-extrabold">
-              {month && month?.charAt(0).toUpperCase() + month?.slice(1)} {year ?? ''}
-              </strong>
+              {month && month?.charAt(0).toUpperCase() + month?.slice(1)}{" "}
+              {year ?? ""}
+            </strong>
             {sideMenu.list.map((items, index) => (
               <li
                 key={index + items.id}
                 className="relative cursor-pointer mt-2 first:mt-0 flex items-center justify-start mx-auto text-center"
-                onClick={(event: React.MouseEvent<HTMLLIElement>) =>
-                  setTab(items.id)
-                }
+                onClick={(event: React.MouseEvent<HTMLLIElement>) => {
+                  // Solo actualiza el estado si el id del item es diferente al tab actual
+                  if (items.id !== tab) {
+                    setTab(items.id);
+                  }
+                }}
               >
                 {items.name}
               </li>
             ))}
           </ul>
         </div>
-        <div className="relative col-span-full max-w-full  bg-orange-500/50 ">
+        <div className="relative col-span-full max-w-full  bg-orange-500/0 ">
           <TailwindGrid>
-            <main className="self-center col-start-1 lg:col-start-3 col-end-5 md:col-end-9 lg:col-end-13 w-full flex flex-col bg-red-300">
-              <div className="flex-col justify-start items-start gap-4 inline-flex lg:pt-[1.5vw]">
-                <h3 className="text-center w-full font-bold text-4xl">
+            <main className="self-center col-start-1 lg:col-start-3 col-end-5 md:col-end-9 lg:col-end-13 w-full flex flex-col bg-red-300/0 justify-center items-center">
+              <div className="flex-col center gap-4 inline-flex lg:pt-[1.5vw] justify-center items-center">
+                <h3 className="text-center w-full font-bold text-4xl ">
                   {sideMenu.list.find((item) => item.id === tab)?.name}
                 </h3>
-                <CardsPressable/>
+                <div className="flex w-full flex-col items-center">
+                  <Tabs aria-label="Options">
+                    <Tab key="posters" title="Cartelería">
+                      <div className="flex w-full flex-col items-center">
+                        <Tabs aria-label="Options">
+                          <Tab key="es" title="Español">
+                            <CardsPressable list={postersCardsES} lang={"es"} />
+                          </Tab>
+                          <Tab key="ca" title="Catalán">
+                            <CardsPressable list={postersCardsCA} lang={"ca"} />
+                          </Tab>
+                        </Tabs>
+                      </div>
+                    </Tab>
+                    <Tab key="stoppers" title="Stopper">
+                      <Card>
+                        <CardBody>
+                          Ut enim ad minim veniam, quis nostrud exercitation
+                          ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in
+                          voluptate velit esse cillum dolore eu fugiat nulla
+                          pariatur.
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                    <Tab key="tests" title="Tests">
+                      <Card>
+                        <CardBody>
+                          Excepteur sint occaecat cupidatat non proident, sunt
+                          in culpa qui officia deserunt mollit anim id est
+                          laborum.
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                    <Tab key="cards" title="Tarjetas">
+                      <Card>
+                        <CardBody>
+                          Excepteur sint occaecat cupidatat non proident, sunt
+                          in culpa qui officia deserunt mollit anim id est
+                          laborum.
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                  </Tabs>
+                </div>
               </div>
             </main>
           </TailwindGrid>
--- components/cards/Cards.tsx
+++ components/cards/Cards.tsx
@@ -1,77 +1,96 @@
 import React from "react";
-import { Button, Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Button, Card, CardFooter, Image } from "@nextui-org/react";
 
-export default function CardsPressable() {
-  const list = [
-    {
-      title: "Orange",
-      img: "/images/fruit-1.jpeg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
-      price: "$12.20",
-    },
-  ];
+export default function CardsPressable({
+  list,
+  lang,
+}: {
+  list: Array<any>;
+  lang: string;
+}) {
+  const renderButtons = (item: any) => {
+    return Object.keys(item.files).map((fileType: string, index: number) => {
+      const file = item.files[fileType];
+
+      return (
+        <Button
+          className="text-tiny text-white bg-gray-700 m-1"
+          variant="flat"
+          key={index}
+          color="default"
+          radius="lg"
+          size="sm"
+          onClick={() => {
+            window.open(file.download, "_blank");
+          }}
+        >
+          {fileType}
+        </Button>
+      );
+    });
+  };
 
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-      {list.map((item, index) => (
-        <Card isFooterBlurred radius="lg" className="border-none" key={index+item.title}>
-          <Image
-            alt={item.title}
-            className="object-cover"
-            height={200}
-            src={item.img}
-            width={200}
-          />
-          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <b className="text-tiny text-white/80">
-              {item.title}
-            </b>
-            <Button
-              className="text-tiny text-white bg-black/20"
-              variant="flat"
-              color="default"
-              radius="lg"
-              size="sm"
-            >
-              {item.price}
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+    <div className="w-full">
+      <div className="gap-2 flex flex-row flex-wrap items-start justify-center">
+        <h3 className="text-center w-full font-bold text-2xl mt-2">
+          {lang === "es" ? "Acciones Principales" : "Accions Principals"}
+        </h3>
+
+        {list &&
+          list
+            .filter((item) => Object.keys(item.files).length === 4)
+            .map((item: any, index: number) => (
+              <CustomCard
+                key={index}
+                item={item}
+                renderButtons={renderButtons}
+              />
+            ))}
+      </div>
+      <div className="gap-2 flex flex-row flex-wrap items-start justify-center">
+        <h3 className="text-center w-full font-bold text-2xl mt-4">
+          {lang === "es" ? "Acciones Secundarias" : "Accions Secundàries"}
+        </h3>
+        {list &&
+          list
+            .filter((item) => Object.keys(item.files).length === 2)
+            .map((item: any, index: number) => (
+              <CustomCard
+                key={index}
+                item={item}
+                renderButtons={renderButtons}
+              />
+            ))}
+      </div>
     </div>
   );
 }
+
+const CustomCard = ({
+  item,
+  renderButtons,
+}: {
+  item: any;
+  renderButtons: any;
+}) => {
+  return (
+    <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none gap-2 px-2 py-3 flex items-center justify-center col-span-1 bg-white"
+      key={item.name}
+    >
+      <Image
+        alt={item.name}
+        className="object-cover border-gray-700/20 border-1 shadow-sm w-52"
+        height={200}
+        src={item.files.A5.imgEmbed}
+        width={200}
+      />
+      <CardFooter className="flex mx-auto flex-row flex-wrap self-start justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl rounded-large bottom-1 w-52 shadow-small z-10">
+        {renderButtons(item)}
+      </CardFooter>
+    </Card>
+  );
+};
