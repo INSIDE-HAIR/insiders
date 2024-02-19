a4d3df56ce1f5dffa5da8d3b55415fa5f58691e2
¨LuisEUM¨ <luiseum95@gmail.com>
2024-02-19 13:12:31 +0100

    nextauth route problem 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
 export { GET, POST } from "@/auth"
-// export const runtime = "edge" // optional but Not supported yet until we have the serverless version of mongodb
+export const runtime = "edge" // optional but Not supported yet until we have the serverless version of mongodb
