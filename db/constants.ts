0b09284bfbdf7faf0ee1c4bd489a9dc20a86d0fe
¨LuisEUM¨ <luiseum95@gmail.com>
2024-04-16 10:51:38 +0200

    Logotipos and Tarjetas

--- db/constants.ts
+++ db/constants.ts
@@ -26,7 +26,7 @@ export const filesCodes = {
   "0002": "Guías",
   "0500": "Filtros de Instagram",
   "6969": "SMS/WhatsApp",
-  "0009" : "Logos",
+  "0009" : "Logotipos para Camisetas y Bolsas",
   "0003" : "Presentaciones",
   // más códigos según sea necesario
 };
