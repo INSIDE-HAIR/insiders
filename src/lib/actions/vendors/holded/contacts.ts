0746b3bb2e32b1fc2e141162a2017f9b4a0cf4dd
¨LuisEUM¨ <luiseum95@gmail.com>
2024-08-23 10:57:35 +0200

    Problem with function structure production deleting working with createOrUpdateCurrent function

--- src/lib/actions/vendors/holded/contacts.ts
+++ src/lib/actions/vendors/holded/contacts.ts
@@ -2,12 +2,12 @@
 "use server";
 
 const getBaseUrl = () => {
-  return process.env.BASE_URL || "http://localhost:3000"; // Define la base URL, ajusta según sea necesario
+  return process.env.BASE_URL || "http://localhost:3000/api"; // Define la base URL, ajusta según sea necesario
 };
 
 export const getListHoldedContacts = async () => {
   const baseUrl = getBaseUrl();
-  const response = await fetch(`${baseUrl}/api/vendor/holded/contacts`);
+  const response = await fetch(`${baseUrl}/vendor/holded/contacts`);
   const data = await response.json();
   console.log("Fetched contacts data:", data); // Log the data
 
@@ -17,7 +17,7 @@ export const getListHoldedContacts = async () => {
 export const getHoldedContactById = async (contactId: string) => {
   const baseUrl = getBaseUrl();
   const response = await fetch(
-    `${baseUrl}/api/vendor/holded/contacts/${contactId}`
+    `${baseUrl}/vendor/holded/contacts/${contactId}`
   );
   const data = await response.json();
 
