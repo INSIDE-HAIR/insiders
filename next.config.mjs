940dd24d75200c308d9e2e4585cc0bed90770501
¨LuisEUM¨ <luiseum95@gmail.com>
2024-08-27 17:53:32 +0200

    image unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       { hostname: "localhost" },
       { hostname: "images.unsplash.com" },
