e6ac9d6f997bbe974d4c9959b8b7cff7e2514482
¨LuisEUM¨ <luiseum95@gmail.com>
2024-08-28 11:24:20 +0200

    fixing api route

--- src/app/[lang]/insiders/(dashboard)/admin/users/page.tsx
+++ src/app/[lang]/insiders/(dashboard)/admin/users/page.tsx
@@ -22,7 +22,7 @@ export default function UsersPage() {
       setIsLoading(true);
       const response = await fetch(
         `${
-          process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api"
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
         }/users`,
         { method: "GET" }
       );
