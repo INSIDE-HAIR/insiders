093d1d5acf98ebe774e7fd47a719c29c31120c8a
¨LuisEUM¨ <sistemas@insidesalons.com>
2024-10-02 10:03:05 +0200

    Fixed Video

--- src/components/ui/videos-players/videos-players.tsx
+++ src/components/ui/videos-players/videos-players.tsx
@@ -2,17 +2,18 @@ import { ComponentsProps } from "@/src/lib/types/components-schemas";
 
 function VideosPlayers({ item, index }: ComponentsProps) {
   return (
-    <div className="firt:mt-0 mb-4 mx-2 md:mx-14 lg:mx-32 aspect-video rounded-3xl overflow-hidden flex w-full  max-w-3xl">
-      <iframe
-        className="aspect-video"
-        width="100%"
-        height="100%"
-        src={item.url}
-        title={item.title}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </div>
+    <>
+      <div className='first:mt-0 mb-4 aspect-video rounded-3xl overflow-hidden flex w-screen max-w-3xl relative '>
+        <iframe
+          width='100%'
+          height='100%'
+          src={item.url}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          className='absolute top-0 left-0 w-full h-full'
+          title={item.title}
+        ></iframe>
+      </div>
+    </>
   );
 }
 
