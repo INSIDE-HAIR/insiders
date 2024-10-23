3fd3c5c9858a534f39c9005114c89ab77f395048
¨LuisEUM¨ <sistemas@insidesalons.com>
2024-10-23 18:27:12 +0200

    Changing UX UI

--- src/components/ui/buttons/button.tsx
+++ src/components/ui/buttons/button.tsx
@@ -5,7 +5,7 @@ function button({ index, item }: ComponentsProps) {
   return (
     <Button
       variant="faded"
-      className="flex bg-gray-700 text-white py-2 px-4 rounded-full mx-auto mb-4"
+      className="flex bg-zinc-900 text-white py-2 px-4 rounded-none border-none mx-auto mb-4"
       style={{ order: item.order || index }}
       onClick={() => {
         window.open(item && item.url, "_blank");
