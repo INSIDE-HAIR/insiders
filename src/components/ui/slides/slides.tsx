d242b232aa6881fdc9443f7cf36a1fe1b275648b
¨LuisEUM¨ <sistemas@insidesalons.com>
2024-10-29 13:04:03 +0100

    Sliders uploaded

--- src/components/ui/slides/slides.tsx
+++ src/components/ui/slides/slides.tsx
@@ -5,7 +5,7 @@ export default function Slides({ index, item }: ComponentsProps) {
     item.active && (
       <iframe
         src={item.url}
-        className="border-2 border-zinc-200 w-full overflow-hidden max-w-full bg-zinc-300 mb-4"
+        className="border-2 border-zinc-200 lg:min-w-[765.789px] w-full overflow-hidden max-w-full bg-zinc-300 mb-4"
         width="750"
         height="460"
         style={{ order: item.order || index }}
